fe7beb00c9ecccb634543b90d28d43109c9e791a
EgidijusRumsa <you@example.comegidijus.rumsa@gmail.com>
2019-04-19 20:51:51 +0300

    Google Maps API

---
+++ js/js_modify.js
@@ -0,0 +1,13 @@
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 54.710067, lng: 25.292306};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    
+
