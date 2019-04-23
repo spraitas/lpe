3a3244aed2abedcf003f5b9854e34ea55ac4f943
Povilas Raciunas <>
2019-04-23 19:03:45 +0300

    changes to homepage

--- js/main.js
+++ js/main.js
@@ -43,3 +43,16 @@ var chart = new Chart(ctx, {
     // Configuration options go here
     options: {}
 });
+
+
+$('.items a').on('click', function() {
+  var $this = $(this),
+      $bc = $('<div class="item"></div>');
+
+  $this.parents('li').each(function(n, li) {
+      var $a = $(li).children('a').clone();
+      $bc.prepend(' / ', $a);
+  });
+    $('.breadcrumb').html( $bc.prepend('<a href="#home">Home</a>') );
+    return false;
+}) 
