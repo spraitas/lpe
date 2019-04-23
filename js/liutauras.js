dd7050b517bdad96ad4c2b7f780b759da054e8cc
spraitas <48630865+spraitas@users.noreply.github.com>
2019-04-23 20:15:03 +0300

    issikeliau js is main  + sudejau cardus i about us

---
+++ js/liutauras.js
@@ -0,0 +1,50 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Isgerto alaus kiekis',
+            backgroundColor: ["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],
+            borderColor: ["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],
+            borderWidth: 1,
+            data: [1, 10, 5, 12, 20, 13, 15]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
+// Load google charts
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
+
+                // Draw the chart and set the chart values
+                function drawChart() {
+                  var data = google.visualization.arrayToDataTable([
+                  ['Task', 'Hours per Day'],
+                  ['Darbas', 0],
+                  ['Maisto gaminimas', 2],
+                  ['Kompiuteriniai zaidimai', 4],
+                  ['Sportas', 0],
+                  ['Darzo prieziura', 4],
+                  ['Miegas', 7],
+                  ['Bimbinejimas', 2],
+                 
+                 
+                    ]);
+
+                  // Optional; add a title and set the width and height of the chart
+                  var options = {
+                    backgroundColor: 'transparent',
+                    'title':'Mano klasikine diena', 'width':650, 'height':400};
+
+              // Display the chart inside the <div> element with id="piechart"
+              var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+              chart.draw(data, options);
+            }
--- js/main.js
+++ js/main.js
@@ -23,26 +23,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Isgerto alaus kiekis',
-            backgroundColor: ["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],
-            borderColor: ["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],
-            borderWidth: 1,
-            data: [1, 10, 5, 12, 20, 13, 15]
-        }]
-    },
 
-    // Configuration options go here
-    options: {}
-});
+
 
 
 $('.items a').on('click', function() {
@@ -56,6 +39,9 @@ $('.items a').on('click', function() {
     $('.breadcrumb').html( $bc.prepend('<a href="#home">Home</a>') );
     return false;
 }) 
+
+
+
 
 // Initialize and add the map
 function initMap() {
