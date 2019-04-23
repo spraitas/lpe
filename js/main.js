bdf1d36b41f23436213f992426b9f72de644a777
Povilas Raciunas <>
2019-04-23 19:45:50 +0300

    crumbs

--- js/main.js
+++ js/main.js
@@ -45,18 +45,6 @@ var chart = new Chart(ctx, {
 });
 
 
-$('.items a').on('click', function() {
-  var $this = $(this),
-      $bc = $('<div class="item"></div>');
-
-  $this.parents('li').each(function(n, li) {
-      var $a = $(li).children('a').clone();
-      $bc.prepend(' / ', $a);
-  });
-    $('.breadcrumb').html( $bc.prepend('<a href="#home">Home</a>') );
-    return false;
-}) 
-
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
